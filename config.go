3d526c4b2458b633fc683e4928443bd91797ceab
Kim Tao <kim.tao@mongodb.com>
2020-06-03 12:28:27 -0400

    MAKE-1350: do not prematurely filter jobs whose DispatchBy has been exceeded (#8)

--- config.go
+++ config.go
@@ -43,12 +43,11 @@ func (c *Configuration) Validate() error {
 
 func (c *Configuration) GetQueueOptions() queue.MongoDBOptions {
 	return queue.MongoDBOptions{
-		URI:             c.MongoDBURI,
-		DB:              c.DatabaseName,
-		Priority:        true,
-		CheckDispatchBy: true,
-		Format:          amboy.BSON2,
-		WaitInterval:    time.Second,
+		URI:          c.MongoDBURI,
+		DB:           c.DatabaseName,
+		Priority:     true,
+		Format:       amboy.BSON2,
+		WaitInterval: time.Second,
 	}
 }
 
